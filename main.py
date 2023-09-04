c090067faec36300d917ff34c5d3e600173a9474
Anthea Chen <anxuec2@illinois.edu>
2023-09-04 02:07:10 -0500

    clean up

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-from craigslistscraper import CraigslistSearches, Searches
-import time
-
+from craigslistscraper import CraigslistSearches
 
 def main():
     """
@@ -16,13 +14,10 @@ def main():
     cities = ['chicago']
     filters = ['&postedToday=1']
 
-    # some examples of what can be done
     # TODO: Use domain builder to add filters to searches
     # TODO: Add Post detail filtering
     SEARCH = CraigslistSearches('https://chicago.craigslist.org/search/sss?query=apple&bundleDuplicates=1&postedToday=1')
-
-    import IPython; IPython.embed()
-
+    SEARCH.display()
 
 if __name__ == '__main__':
     main()
